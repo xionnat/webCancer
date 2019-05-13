34cbda90398abae6ca5f3028a70d0f26b6950920
Natália Sacramento <xionnat@gmail.com>
2019-05-13 09:45:50 -0300

    Create notificacao.php

---
+++ web/PHP/notificacao.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Ren-con</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<nav>
+			  <a href="...">Perfil</a> |
+			  <a href="...">Notificações</a> |
+			  <a href="...">Solicitações de Amizades</a> |
+			  <a href="...">Configurações</a>
+			  <input type="text" name="search">
+			  <input type="button" name="pesquisar" value="Pesquisar"> <!-- colocar imagem de uma ampulheta-->
+			  <a href="...">Sair</a> <!--configurar logout-->
+			</nav><br>
+		<div class="container">
+			<table width="100%"  border="1">
+				<tr>
+					<td width="15%"> Nome do ser abissal <img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="150px"></td> <!--foto de perfil da pessoa-->
+					<td>
+						<li>
+							<ul><a href="..."><img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="40px"> Escoria 1 comentou no seu post</a></ul>
+						</li>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<fieldset>
+							<legend>Escórias</legend>
+							<img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="30px"> <a href="...">Escoria 1</a>
+						</fieldset><!--lista de amigos-->
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
