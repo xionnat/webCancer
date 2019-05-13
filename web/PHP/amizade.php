f0b6fab3b56987ba50fcec0a80e44ea50e20d3f1
Natália Sacramento <xionnat@gmail.com>
2019-05-13 09:44:12 -0300

    Create amizade.php

---
+++ web/PHP/amizade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Ren-con</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<nav>
+			  <a href="...">Perfil</a> |
+			  <a href="...">Notificações</a> |
+			  <a href="...">Solicitações de Amizades</a> |
+			  <a href="...">Configurações</a>
+			  <input type="text" name="search">
+			  <input type="button" name="pesquisar" value="Pesquisar"> <!-- colocar imagem de uma ampulheta-->
+			  <a href="...">Sair</a> <!--configurar logout-->
+			</nav><br>
+		<div class="container">
+			<table width="100%"  border="1">
+				<tr>
+						<td width="15%"> Nome do ser abissal <img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="150px"></td> <!--foto de perfil da pessoa-->
+						<td>
+							<li>
+								<ul>
+									<img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="40px"><a href="">Nome do escoria</a>
+									<input type="button" name="aceitar" value="aceitar escória">
+									<input type="button" name="recusar" value="recusar escória">
+								</ul>
+							</li>
+						</td>
+				</tr>
+				<tr>
+						<td>
+							<fieldset>
+								<legend>Escórias</legend>
+								<img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="30px"> <a href="...">Escoria 1</a>
+							</fieldset><!--lista de amigos-->
+						</td>
+					</tr>
+			</table>
+		</div>
+	</body>
+</html>
