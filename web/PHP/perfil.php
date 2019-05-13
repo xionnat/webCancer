caeb22e6258baa71cab3fa20a77f2f133191d0c6
Natália Sacramento <xionnat@gmail.com>
2019-05-13 09:47:58 -0300

    Create perfil.php

---
+++ web/PHP/perfil.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Ren-chon</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<nav>
+			  <a href="...">Perfil</a> |
+			  <a href="...">Notificações</a> |
+			  <a href="...">Solicitações de Amizades</a> |
+			  <a href="...">Configurações</a>
+			  <input type="text" name="search">
+			  <input type="button" name="pesquisar" value="Pesquisar"> <!-- colocar imagem de uma ampulheta-->
+			  <a href="...">Sair</a> <!--configurar logout-->
+			</nav><br>
+			<div class="container">
+				<table width="100%"  border="1">
+					<tr>
+						<td width="15%"> Nome do ser abissal <img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="150px"></td> <!--foto de perfil da pessoa-->
+						<td>
+							<fieldset>
+								<table>
+									<tr>
+										<td rowspan="2"><img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="100px"></td>
+										<td><h2>Nome do escoria</h2></td>
+									</tr>
+									<tr>
+										<td><a href=""><p>Amigos</p></a></td>
+									</tr>
+								</table>
+							</fieldset><br><br>
+							<table>
+								<tr>
+									<td rowspan="2"> <img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="40px"> </td>
+									<td><a href="..."> Nome do ser</a></td>
+									<td rowspan="2">deletar</td><!--colocar uma lixeirinha-->
+								</tr>
+								<tr>
+									<td>horario do post</td>									
+								</tr>
+							</table>
+							<p>	Pudim com chocolate é bom </p>
+							<input type="button" name="gostei" value="gostei">
+							<input type="button" name="gostei" value="comentar">
+							<!-- carregar mais posts-->
+							<h2><center>Não há mais nada para ver</center></p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<fieldset>
+								<legend>Escórias</legend>
+								<img src="https://pbs.twimg.com/profile_images/790963803043209217/dDKfKuFy.jpg" width="30px"> <a href="...">Escoria 1</a>
+							</fieldset><!--lista de amigos-->
+						</td>
+					</tr>
+				</table>
+			</div>
+		</body>
+	</html>
